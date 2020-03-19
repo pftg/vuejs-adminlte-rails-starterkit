441ab1e4345be2fa10af407c9ab598a727121d3f
Paul Keen <62-pkeen@users.noreply.rtfm.corsis.com>
2020-03-20 00:06:40 +0200

    Install AdminLTE Theme

---
+++ app/javascript/initializers/adminlte.js
@@ -0,0 +1,6 @@
+import '../styles/adminlte.scss'
+
+import "@fortawesome/fontawesome-free";
+import 'jquery/src/jquery.js'
+import 'popper.js'
+import 'admin-lte/build/js/AdminLTE';
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ import 'serviceworker-companion'
 
 import 'initializers/polyfills'
 import 'initializers/turbolinks'
+import 'initializers/adminlte'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
