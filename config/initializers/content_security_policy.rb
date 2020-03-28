64efeace7822c79941f2aaea6846e53351d111b9
Paul Keen <62-pkeen@users.noreply.rtfm.corsis.com>
2020-03-28 18:50:22 +0200

    REmoves redundant unsafe

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -30,7 +30,7 @@
 # Rails.application.config.content_security_policy_report_only = true
 
   Rails.application.config.content_security_policy do |policy|
-    policy.script_src :self, :https, :unsafe_eval
+    policy.script_src :self, :https
 
     if Rails.env.development? || Rails.env.test?
       policy.connect_src :self, :https, 'http://localhost:3035', 'ws://localhost:3035'
